893067c8e2476d1e564df494f4c54de91bb35655
Gayan Wijesuriya <67247345+Gayoon1234@users.noreply.github.com>
2023-03-13 13:49:08 +1100

    Update Decoder.java

--- Decoder.java
+++ Decoder.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Decoder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Decoder f = new Decoder();
 		f.UserInput();
 		
